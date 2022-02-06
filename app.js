7fb5405261881291251a9db78127d0742c5deef6
Leoarguinzones <leot5865@gmail.com>
2022-02-05 21:54:02 -0300

    Subo archivo base hasta clase 14

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const patheoPublic = path.join(__dirname, "./public");
+app.use(express.static(patheoPublic));
+
+app.listen(3000, () => {
+    console.log("Servidor 3000 corriendo");
+})
+
+app.get("/", (req, res) => {
+    let patheoGet = path.resolve(__dirname, "./views/home.html");
+    res.sendFile(patheoGet);
+})
