c1889912d4fa143a63afe955ec17192aa955483b
leot5865@gmail.com <leot5865@gmail.com>
2022-02-18 22:40:01 -0300

    subo cambios que no guarde para el heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const patheoPublic = path.join(__dirname, "./public");
 app.use(express.static(patheoPublic));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, function(){
     console.log("Servidor 3000 corriendo");
 })
 
